0e563c6bfeaef2823105d94a33e8d3088e31ba45
Harbinderminhas <harryminhas764@gmail.com>
2020-09-19 15:32:17 -0700

    Add files via upload

---
+++ CountWords.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        //counting the words program in java
+
+        System.out.println("Enter the Word:");
+        Scanner s1 = new Scanner(System.in);
+        String phrase = s1.nextLine();
+
+
+
+        System.out.println(phrase.length());
+
+    }
+}
+
